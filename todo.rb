2fb30f3fea46c9641643756f2f30f02f86e55a80
Douglas Vidotto Carvalho <doug.vidotto@gmail.com>
2018-02-25 22:16:39 -0300

    Showing detail of the list

--- todo.rb
+++ todo.rb
@@ -1,7 +1,7 @@
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
-
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/content_for'
+require 'tilt/erubis'
 
 configure do
   enable :sessions
@@ -9,26 +9,52 @@ configure do
 end
 
 before do
-  session[:lists] ||= [] # The same as session[:list] || session[:list] = [] (a || a = b)
+  # The same as session[:list] || session[:list] = [] (a || a = b)
+  session[:lists] ||= []
 end
 
-get "/" do
-  redirect "/lists"
+get '/' do
+  redirect '/lists'
 end
 
 # View all the existent lists
-get "/lists" do
+get '/lists' do
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
 # Render new list form
-get "/lists/new" do
+get '/lists/new' do
   erb :new_list, layout: :layout
 end
 
+# Return an error message if the name is invalid.
+# Return nil if the name is valid.
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    'List name must be between 1 and 100 characters.'
+  elsif session[:lists].any? { |list| list[:name] == name }
+    'List name must be unique.'
+  end
+end
+
 # Create a new list
-post "/lists" do
-  session[:lists] << {name: params[:list_name], todos: []}
-  redirect "/lists"
+post '/lists' do
+  list_name = params[:list_name].strip
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = 'The list has been created.'
+    redirect '/lists'
+  end
+end
+
+
+get '/lists/:idx' do
+  list_idx = params[:idx].to_i
+  @list_detail = session[:lists][list_idx]
+  erb :todo, layout: :layout
 end
